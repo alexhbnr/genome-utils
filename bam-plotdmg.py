9230a9e1cd17ea9d32c1731c6375cc3a161bd752
Martin Petr <git@bodkan.net>
2016-03-16 17:54:59 +0100

    Rewrite the DNA damage analysis script from the ground up

--- bam-plotdmg.py
+++ bam-plotdmg.py
@@ -5,51 +5,72 @@ import pysam
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+sns.set_style('whitegrid')
 
 
-bases = 'ACGT'
+BASES = 'ACGT'
+
+# translation dict for converting DNA sequence into reverse complement
+TR_TABLE = str.maketrans(BASES, BASES[::-1])
 
 
-def mismatch_key(b1, b2):
-    '''Create a string X-Y to be used as a key to a data frame.'''
-    return '-'.join((b1, b2))
+def mismatch(b1, b2):
+    '''Create a string "X-Y" to be used as a key to a data frame.'''
+    return b1 + '-' + b2
 
 
-def mismatch_combinations():
-    '''Generate all combination of mismatches to be used as keys
-    to a DataFrame.
+def mismatch_combs():
+    '''Generate all combinations of possible mismatches of DNA bases.'''
+    return [mismatch(b1, b2) for b1 in BASES
+                             for b2 in BASES if b1 != b2]
+
+
+def count_mismatches(mism5_count, mism3_count, ref5_count, ref3_count,
+                     ref_bases, read_bases, len_limit):
+    '''Count the number of mismatches along a read as well as the
+    composition of reference bases.
     '''
-    mismatch_comb = [mismatch_key(b1, b2) for b1 in bases
-                                          for b2 in bases if b1 != b2]
-    return mismatch_comb
+    read_len = len(read_bases)
+
+    for pos, (b_ref, b_read) in enumerate(zip(ref_bases, read_bases)):
+        rev_pos = read_len - pos - 1
+
+        # skip the position if the reference does not have a valid base
+        if b_ref not in BASES or b_read not in BASES: continue
+
+        # increment counters of ref alleles at a position
+        if pos     < len_limit: ref5_count.ix[pos,     b_ref] += 1
+        if rev_pos < len_limit: ref3_count.ix[rev_pos, b_ref] += 1
+
+        # if there's a mismatch on this position, increment the counter
+        if b_ref != b_read:
+            key = mismatch(b_ref, b_read)
+
+            if pos     < len_limit: mism5_count.ix[pos, key] += 1
+            if rev_pos < len_limit: mism3_count.ix[len_limit - rev_pos - 1, key] += 1
 
 
-def count_mismatches_in_read(mismatch_table, ref_bases, read_bases, len_limit):
-        '''Calculate the number of mismatches along a read and update
-        the mismatch table. Do not analyze sites within a read which are
-        further from the beginning than len_limit.
-        '''
-        for pos, (b_ref, b_read) in enumerate(zip(ref_bases, read_bases)):
-            # skip the rest of the read if already beyond the limit
-            if pos >= len_limit: break
-
-            # skip the base if it's not A, C, G or T
-            if b_ref not in bases or b_read not in bases: continue
-
-            # if there's a mismatch on this position, increment the counter
-            if b_ref != b_read:
-                mismatch_table.ix[pos, mismatch_key(b_ref, b_read)] += 1
+def calc_frequencies(mism_counts, ref_count):
+    ''' Calculate substitution frequencies from counts of observed
+    substitution patterns and counts of reference bases at each
+    position.
+    '''
+    return pd.concat((mism_counts.filter(regex=r'^' + b).divide(ref_count[b], axis=0)
+                      for b in BASES), axis=1)
 
 
-def analyze_bam(bam_path, len_limit=20):
+def analyze_bam(bam_path, len_limit=15):
     '''Count the number of occurences of different substitutions in all
     reads as well as the total number of reads and the number of unmapped
     reads.
     '''   
-    # initialize counters of reads and mismatch-counting data frames
-    n_fwd_reads, n_rev_reads = 0, 0
-    fwd_mismatches = pd.DataFrame(0, columns=mismatch_combinations(), index=range(len_limit))
-    rev_mismatches = pd.DataFrame(0, columns=mismatch_combinations(), index=range(len_limit))
+    # initialize counters of mismatches from 5' and 3' ends of reads
+    mism5_counts = pd.DataFrame(0, columns=mismatch_combs(), index=range(len_limit))
+    mism3_counts = pd.DataFrame(0, columns=mismatch_combs(), index=range(len_limit))
+
+    # initialize counters of reference bases from the 5' and 3' ends of reads
+    ref5_counts = pd.DataFrame(0, columns=list(BASES), index=range(len_limit))
+    ref3_counts = pd.DataFrame(0, columns=list(BASES), index=range(len_limit))
     
     ref_genome = '/mnt/solexa/Genomes/hg19_evan/whole_genome.fa'
 
@@ -57,70 +78,59 @@ def analyze_bam(bam_path, len_limit=20):
         fastaf = pysam.FastaFile(ref_genome)
         
         for read in bamf:
-            ref_bases = list(fastaf.fetch(reference=bamf.getrname(read.reference_id),
-                                          start=read.reference_start,
-                                          end=read.reference_end))
-            read_bases = list(read.query_alignment_sequence)
-            mismatch_table = fwd_mismatches
+            ref_bases = fastaf.fetch(read.reference_name,
+                                     read.reference_start,
+                                     read.reference_end)
+            read_bases = read.query_alignment_sequence
 
-            # if the read is in a reversed orientation, reverse the fetched
-            # sequences and change reference to the table to be updated
             if read.is_reverse:
-                read_bases = reversed(read_bases)
-                ref_bases = reversed(ref_bases)
-                mismatch_table = rev_mismatches
+                ref_bases = ref_bases[::-1].translate(TR_TABLE)
+                read_bases = read_bases[::-1].translate(TR_TABLE)
 
-            # update the mismatch table based on data from this read
-            count_mismatches_in_read(mismatch_table, ref_bases, read_bases, len_limit)
-            
-            if read.is_reverse:
-                n_rev_reads += 1
-            else:
-                n_fwd_reads += 1
-    
-    
-    fwd_mismatches = fwd_mismatches.div(n_fwd_reads) * 100
-    rev_mismatches = rev_mismatches.div(n_rev_reads) * 100
-    
-    print('Total number of forward reads: {}'.format(n_fwd_reads))
-    print('Total number of reverse reads: {}'.format(n_rev_reads))
-    
-    return fwd_mismatches, rev_mismatches[::-1]
+            count_mismatches(mism5_counts, mism3_counts,
+                             ref5_counts, ref3_counts,
+                             ref_bases, read_bases, len_limit)
+
+    mism5_freqs = calc_frequencies(mism5_counts, ref5_counts)
+    mism3_freqs = calc_frequencies(mism3_counts, ref3_counts)
+
+    return mism5_freqs, mism3_freqs
 
 
-def plot_mismatches(mismatch_table, title, ylim=30):
-    '''Plot the counts of mismatches in a specified orientation.'''
+def plot_mismatches(mism_freqs, read_end):
+    '''Plot the frequencies of mismatches.'''
     fig = plt.figure()
 
-    mismatch_table.plot(linewidth=3, figsize=(13, 9), fontsize=15)
-    plt.title(title, fontsize=20)
-    plt.xlabel('position from the beginning of the read [bp]', fontsize=16)
+    end_str = str(read_end) + '\' end'
+
+    mism_freqs.plot(linewidth=3, figsize=(13, 9), fontsize=15)
+    plt.title('mismatches from the ' + end_str, fontsize=20)
+    plt.xlabel('position from the ' + end_str, fontsize=16)
     plt.ylabel('fraction of mismatch type at a position [%]', fontsize=16)
-    plt.xticks(range(len(mismatch_table)))
-    plt.ylim(0, ylim)
+    plt.xticks(range(0, len(mism_freqs), 5))
 
     return fig
 
     
-def save_mismatches(mismatch_table, strand, ylim, output_dir, bam_file, len_limit):
-    title = strand + ' strand ' + '-- ' + os.path.basename(bam_file)
-    fig = plot_mismatches(mismatch_table, title, ylim)
-    output_file = strand + '_damage_' + os.path.basename(bam_file) + '.png'
+def save_mismatches(mismatch_table, read_end, output_dir, bam_file, len_limit):
+    fig = plot_mismatches(mismatch_table, read_end)
+
+    end_str = str(read_end) + '_prime_end'
+
+    output_file = 'subst_from_' + end_str + '__' + os.path.basename(bam_file) + '.png'
     plt.savefig(os.path.join(output_dir, output_file))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('Analyze damage patterns in a BAM file')
     parser.add_argument('--bam', help='BAM file to analyze', required=True)
-    parser.add_argument('--len_limit', help='How deep into a read to look for damage?',
-                        type=int, default=20)
-    parser.add_argument('--ylim', help='Upper limit on percentage in the plot',
-                        type=int, default=20)
+    parser.add_argument('--len_limit', help='How deep into reads to look '
+                        'for damage?', type=int, default=30)
     parser.add_argument('--dir', help='Where to put the plot?', default='.')
     parser.add_argument('--format', help='Format of the output image',
                         default='png')
     args = parser.parse_args()
 
-    fwd_mismatches, rev_mismatches = analyze_bam(args.bam, args.len_limit)
-    save_mismatches(fwd_mismatches, 'forward', args.ylim, args.dir, args.bam, args.len_limit)
-    save_mismatches(rev_mismatches, 'reverse', args.ylim, args.dir, args.bam, args.len_limit)
+    mism5_freqs, mism3_freqs = analyze_bam(args.bam, args.len_limit)
+    save_mismatches(mism5_freqs, 5, args.dir, args.bam, args.len_limit)
+    save_mismatches(mism3_freqs, 3, args.dir, args.bam, args.len_limit)
